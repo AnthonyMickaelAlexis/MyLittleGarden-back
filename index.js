05c884addecb798d85223bf8e6d6c7eb86d15deb
NordineHadni <nordinehadni@gmail.com>
2022-06-09 14:47:11 +0200

    project initialization

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+// We get express-session to manage user sessions
+const session = require("express-session");
+
+require('dotenv').config()
+
+const router = require('./app/router/index');
+
+const app = express();
+
+app.use(router);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`)
+})
