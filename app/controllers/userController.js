a7353110ba23da7969efb6119b77db36d5017152
Etudiant O'clock <yellownanaki@gmail.com>
2022-06-10 13:06:29 +0200

    using the userdatamapper from middlewares folder in usercontroller

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,21 +1,45 @@
+const userDatamapper = require('../middlewares/user')
+
 const userController = {
 
     //get loginUser
     loginUser(req,res) {
-        res.send('loginUser');
+        try {
+            res.send('loginUser');
+        } catch (err) {
+            console.error(err);
+            res.status(500).send(err.message);
+        }
     },
     //post loginUser
     loginUserConnection(req,res) {
-        res.send('loginUserPost');
+        try {
+            res.send('loginUserPost');
+        } catch (err) {
+            console.error(err);
+            res.status(500).send(err.message);
+        }
     },
     //get registerUser
-    registerUser(req,res) {
-        res.send('registerUser');
+    async registerUser(req,res) {
+        try {
+            await userDatamapper.insert(req.body)
+            res.redirect('/login');
+            res.send('registerUser');
+        } catch (err) {
+            console.error(err);
+            res.status(500).send(err.message);
+        }
     },
     //post registerUser
     registerUserPost(req,res) {
-        res.send('registerUserPost');
-    },
+        try {
+            res.send('registerUserPost');
+        } catch (err) {
+            console.error(err);
+            res.status(500).send(err.message);
+        }
+    }
     
 };
 
