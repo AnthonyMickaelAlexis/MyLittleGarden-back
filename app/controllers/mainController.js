a060926ee8bc110d867811df675836a0d0e2d2f8
Etudiant O'clock <yellownanaki@gmail.com>
2022-06-10 21:29:10 +0200

    fixing merge conflict with another back-end developer 06-10-nordine with my branch 06-10-micka with saving files modification this time

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const res = require("express/lib/response");
 
 const mainController = {
@@ -6,7 +5,10 @@ const mainController = {
     homePage(_, res) {
         res.send('home');
     },
-
+    async getAllUsers(_, res) {
+        const users = await userDataMapper.findAll();
+        return res.json(users);
+    },
     error404(req, res) {
         res.status(404).render('error404', { url: req.url });
     },
@@ -34,23 +36,3 @@ const mainController = {
 };
 
 module.exports = mainController;
-=======
-const userDataMapper = require('../models/user');
-
-
-const mainController = {
-
-
-
-    async getAllUsers(_, res) {
-        const users = await userDataMapper.findAll();
-        return res.json(users);
-    },
-    // main page
-    homePage(_, res) {
-        res.send('home');
-    }};
-
-
-    module.exports = mainController;
->>>>>>> 06-10-nordine
