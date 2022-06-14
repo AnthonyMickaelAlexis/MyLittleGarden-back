728222a9f309ac540700135f82f5f2b2db323f14
NordineHadni <nordinehadni@gmail.com>
2022-06-14 17:13:19 +0200

    renaming environment variables

--- app/config/db.js
+++ app/config/db.js
@@ -1,7 +1,8 @@
-
-
 const { Client } = require('pg');
-const client = new Client(process.env.PGURL);
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
 client.connect();
 
 module.exports = client;
