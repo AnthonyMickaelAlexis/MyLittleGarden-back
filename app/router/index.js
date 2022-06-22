a37d32d37a6aeaa90442f063d32ce263eed074e8
Etudiant O'clock <yellownanaki@gmail.com>
2022-06-22 17:15:31 +0200

    branch removing tokenauthentification from presenting except on two url to show how it's work for presenting

--- app/router/index.js
+++ app/router/index.js
@@ -12,7 +12,7 @@ const cropController = require('../controllers/cropController');
 const user_has_plant = require('../models/user_has_plant');
 
 router.get('/home', mainController.homePage);
-router.get('/profil/users', checkTokenMiddleware, userController.getAllUsers);
+router.get('/profil/users', userController.getAllUsers);
 // connexion page
 router.get('/login', userController.loginUser);
 router.post('/login', userController.loginUserConnection);
@@ -23,28 +23,28 @@ router.post('/register', userController.registerUserPost);
 
 
 // crop page
-router.get('/crops',checkTokenMiddleware, cropController.getAllCrops);
-router.get('/crop/:id',checkTokenMiddleware, cropController.getOneCrop);
-router.post('/crop',checkTokenMiddleware, cropController.AddOneCrop);
-router.delete('/crop/:id',checkTokenMiddleware, cropController.deleteCrop);
+router.get('/crops', cropController.getAllCrops);
+router.get('/crop/:id', cropController.getOneCrop);
+router.post('/crop', cropController.AddOneCrop);
+router.delete('/crop/:id', cropController.deleteCrop);
 
 // favorite crp list
 
-router.get('/:userid/favori',checkTokenMiddleware, cropController.GetFavoriteListForUser);
-router.post('/:cropid/:userid',checkTokenMiddleware, cropController.AddCropInFavoriteList);
+router.get('/:userid/favori', cropController.GetFavoriteListForUser);
+router.post('/:cropid/:userid', cropController.AddCropInFavoriteList);
 
 
 // member information profil, read, modify and delete
-router.get('/home/profil/:user',checkTokenMiddleware, userController.getUserProfil);
-router.patch('/home/profil/:userid',checkTokenMiddleware, userController.patchUserProfil);
-router.delete('/profil/:user',checkTokenMiddleware, userController.deleteUser);
+router.get('/home/profil/:user', userController.getUserProfil);
+router.patch('/home/profil/:userid', userController.patchUserProfil);
+router.delete('/profil/:user', userController.deleteUser);
 
 // parcel page (main page when the user is connected) read, modify parcel name and delete all crops from the parcel
-router.get('/home/profil/:user/parcel',checkTokenMiddleware, parcelController.getUserParcel);
-router.patch('/home/profil/:user/parcel',checkTokenMiddleware, parcelController.patchUserParcel);
-router.delete('/home/profil/:user/parcel/delete',checkTokenMiddleware, parcelController.deleteParcel);
+router.get('/home/profil/:user/parcel', parcelController.getUserParcel);
+router.patch('/home/profil/:user/parcel', parcelController.patchUserParcel);
+router.delete('/home/profil/:user/parcel/delete', parcelController.deleteParcel);
 
 /* Test routage patch save bdd*/
-router.get('/home/profil/:user/parcelsave',checkTokenMiddleware, parcelController.patchUserParcel);
+router.get('/home/profil/:user/parcelsave', parcelController.patchUserParcel);
 
 module.exports = router;
