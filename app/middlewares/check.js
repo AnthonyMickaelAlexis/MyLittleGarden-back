99c0761a6615fb5b8f5266e6ac8e406b3e27d90c
Etudiant O'clock <yellownanaki@gmail.com>
2022-06-27 10:01:43 +0200

    sorting middlewares with eslint airbnb

--- app/middlewares/check.js
+++ app/middlewares/check.js
@@ -1,60 +1,38 @@
+/* eslint-disable consistent-return */
 const jwt = require('jsonwebtoken');
-
-
-
 
 // Extraction du token
 
-const extractBearer = authorization => {
+const extractBearer = (authorization) => {
+  if (typeof authorization !== 'string') {
+    return false;
+  }
 
-    if(typeof authorization !== 'string'){
-        return false
-    }
+  // on isole le token
 
-    // on isole le token
+  const matches = authorization.match(/(bearer)\s+(\S+)/i);
 
-
-    const matches = authorization.match(/(bearer)\s+(\S+)/i)
-
-    return matches && matches[2]
-
-
-
-}
-
-
-
-
-
-
+  return matches && matches[2];
+};
 
 // Vérification de la présence du token
 
-
-
 const checkTokenMiddleware = (req, res, next) => {
+  const token = req.headers.authorization && extractBearer(req.headers.authorization);
 
-    const token = req.headers.authorization && extractBearer(req.headers.authorization)
+  if (!token) {
+    return res.status(401).json({ message: 'Pas de Token, Pas de route' });
+  }
 
-    if(!token) {
-        return res.status(401).json({ message: 'Pas de Token, Pas de route'})
+  // Vérifie la validité du token
+
+  jwt.verify(token, process.env.JWT_SECRET, (err) => {
+    if (err) {
+      return res.status(401).json({ message: 'Bad Token' });
     }
 
-    // Vérifie la validité du token
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-        if(err){
-            return res.status(401).json({message: 'Bad Token'})
-        }
-
-        next()
-    })
-
-}
-
-
-
-
-
+    next();
+  });
+};
 
 module.exports = checkTokenMiddleware;
--- app/middlewares/nodemailer.js
+++ app/middlewares/nodemailer.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+// eslint-disable-next-line import/no-unresolved
 const smtpTransport = require('nodemailer-smtp-transport');
 
 const transporter = nodemailer.createTransport(smtpTransport({
@@ -6,22 +7,22 @@ const transporter = nodemailer.createTransport(smtpTransport({
   host: 'smtp.gmail.com',
   auth: {
     user: 'alexis.anthony.bx@gmail.com',
-    pass: process.env.GMAIL_PASSWD
-  }
+    pass: process.env.GMAIL_PASSWD,
+  },
 }));
 
 const mailOptions = {
   from: 'alexis.anthony.bx@gmail.com',
   to: 'alexis.anthony.bx@gmail.com',
   subject: 'Sending Email using Node.js[nodemailer]',
-  text: 'That was easy!'
+  text: 'That was easy!',
 };
 
-transporter.sendMail(mailOptions, function(error, info){
+transporter.sendMail(mailOptions, (error, info) => {
   if (error) {
     console.log(error);
   } else {
-    console.log('Email sent: ' + info.response);
+    console.log(`Email sent: ${info.response}`);
   }
 });
 
