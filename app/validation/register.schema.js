29424f671df8c4eb4f808c77bf6b9d68e516f1e8
Etudiant O'clock <yellownanaki@gmail.com>
2022-08-08 13:21:03 +0200

    adding min 4 and max 21 characters on joi password correcting a mistake

--- app/validation/register.schema.js
+++ app/validation/register.schema.js
@@ -20,8 +20,8 @@ module.exports = Joi.object({
 
   password: Joi.string()
     // eslint-disable-next-line prefer-regex-literals
-    .min(3),
-    .max(21),
+    .min('')
+    .max(21)
     .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
 
   confirm_password: Joi.ref('password'),
